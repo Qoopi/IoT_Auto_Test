229c762b9f6d13ed14e9a993a67e733a12b560ac
alex00x6 <alex00x6@gmail.com>
2017-03-27 13:36:22 +0300

    stress scenario (dashboard load)

--- src/test/java/load/PageBootSpeed.java
+++ src/test/java/load/PageBootSpeed.java
@@ -31,61 +31,61 @@ public class PageBootSpeed {
     @Test
     public void checkNewCreds1(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds2(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds3(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds4(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds5(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds6(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds7(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds8(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds9(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
     @Test
     public void checkNewCreds10(){
         RequestManager requestManager = new RequestManager();
-        requestManager.getChart(10000,0);
+        requestManager.getChart(10000,1);
     }
 
 }
