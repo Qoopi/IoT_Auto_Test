1e51e65f1ed83ec4878dfe2c1f361e250603800e
alex00x6 <alex00x6@gmail.com>
2017-03-30 14:55:10 +0300

    CRUD scenario in progress

--- src/main/java/load/RequestManager.java
+++ src/main/java/load/RequestManager.java
@@ -15,7 +15,7 @@ import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
-public class RequestManager extends SignAWSv4{
+public class RequestManager extends RequestTemplates{
     private FileOutputStream fos = null;
 
     public void getChart(int repeats, int timeBetweenRequests){
@@ -28,7 +28,6 @@ public class RequestManager extends SignAWSv4{
 
             createEmptyRequestWithHeaders(authHeaders).addHeaders(standardHeaders).get(url);
             sleep(timeBetweenRequests);
-
         }
     }
 
@@ -42,6 +41,28 @@ public class RequestManager extends SignAWSv4{
         createRequestWithHeaders(standardHeaders, jsonBody).addHeaders(authHeaders).post(url);
     }
 
+    public void notificationRulesRead(){
+        String method = "GET";
+        String url = "https://60sglz9l5h.execute-api.us-east-1.amazonaws.com/dev/rule";
+
+
+        Map<String, String> standardHeaders = standardHeaders();
+        Map<String, String> authHeaders = authHeaders(method, url);
+
+        createEmptyRequestWithHeaders(standardHeaders).addHeaders(authHeaders).get(url);
+    }
+
+    public void notificationRuleUpdate(){
+        String method = "PUT";
+        String url = "https://60sglz9l5h.execute-api.us-east-1.amazonaws.com/dev/rule";
+        String jsonBody = "{\"items\":[{\"id\":\"f1d40e8b-df08-4432-8914-63fccfc9a345\",\"active\":1,\"name\":\"dsfsd\",\"description\":\"\",\"notificationType\":0,\"type\":8,\"phones\":[{\"value\":\"+380632517418\",\"name\":\"me\"}],\"emails\":[{\"value\":\"hom.ossystem@gmail.com\",\"name\":\"My email\"}],\"notifications\":{\"alwaysSend\":false,\"triggered\":5,\"acknowledged\":30,\"globalSettings\":0,\"sms\":0,\"emails\":0},\"equipmentIds\":[\"Thing-090018-0\"],\"channel\":0,\"frq\":0,\"threshold\":0,\"trigger\":\"\",\"operation\":\">=\",\"value\":30,\"period\":0,\"sensor\":\"\"}]}";
+
+        Map<String, String> standardHeaders = standardHeaders();
+        Map<String, String> authHeaders = authHeaders(method, url, jsonBody);
+
+        createRequestWithHeaders(standardHeaders, jsonBody).addHeaders(authHeaders).put(url);
+    }
+
     public void notificationRuleDelete(){
         String method = "DELETE";
         String url = "https://60sglz9l5h.execute-api.us-east-1.amazonaws.com/dev/rule";
@@ -51,6 +72,11 @@ public class RequestManager extends SignAWSv4{
         Map<String, String> authHeaders = authHeaders(method, url, jsonBody);
 
         createRequestWithHeaders(standardHeaders, jsonBody).addHeaders(authHeaders).delete(url);
+    }
+
+
+    public void notificationRuleCRUD(){
+
     }
 
 
@@ -201,104 +227,6 @@ public class RequestManager extends SignAWSv4{
         canvasGPVChartRefreshTemplate(operatingTimeMins, chartUpdate, dashboardInfo, notificationUnread);
     }
 
-
-    private void canvasChartRefreshTemplate(int operatingTimeMins, String chartUpdateUrl, String dashboardInfoUrl, String notificationUnreadUrl){
-        //all requests on start
-        //6 chart update requests every 5 seconds
-        //2 dash info requests every 30 seconds
-        //2 notification requests every 1 min
-        Map<String, String> standardHeaders = standardHeaders();
-        Map<String, String> notificationUnreadHeaders = null;
-        Map<String, String> dashboardInfoHeaders = null;
-        Map<String, String> chartUpdateHeaders = null;
-
-        System.out.println("TIME : HTTP STATUS CODE : RESPONSE TIME : METHOD : URL");
-        System.out.println(LocalDateTime.now() + ": Started");
-        //2 1min requests on start here
-        notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
-        createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
-        createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
-        //2 30 sec requests on start here
-        dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
-        createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
-        createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
-
-        for (int i2 = 0; i2 < operatingTimeMins; i2++) {
-            for (int i1 = 0; i1 < 2; i1++) {
-                for (int i = 0; i < 6; i++) {
-                    //5 sec cycle
-                    //6 requests every 5 sec here (1 sec cut for response)
-                    chartUpdateHeaders = authHeaders("GET", chartUpdateUrl);
-                    createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
-                    createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
-                    createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
-                    createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
-                    createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
-                    createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
-                    sleep(4000);
-                }
-                //30 sec cycle
-                //2 requests every 30 sec here
-                dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
-                createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
-                createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
-            }
-            //1 min cycle
-            //2 requests every 1 min here
-            notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
-            createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
-            createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
-        }
-    }
-
-    private void canvasGPVChartRefreshTemplate(int operatingTimeMins, String chartUpdateUrl, String dashboardInfoUrl, String notificationUnreadUrl){
-        //all requests on start
-        //2 dash info requests every 30 sec
-        //2 notification requests every 1 min
-        //2 chart update requests every 1 min
-        Map<String, String> standardHeaders = standardHeaders();
-        Map<String, String> notificationUnreadHeaders = null;
-        Map<String, String> dashboardInfoHeaders = null;
-        Map<String, String> chartUpdateHeaders = null;
-
-        System.out.println("TIME : HTTP STATUS CODE : RESPONSE TIME : METHOD : URL");
-        System.out.println(LocalDateTime.now() + ": Started");
-        //
-        notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
-        createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
-        createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
-        //
-        dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
-        createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
-        createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
-        //
-        chartUpdateHeaders = authHeaders("GET", chartUpdateUrl);
-        createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
-        createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
-
-        for (int i2 = 0; i2 < operatingTimeMins; i2++) {
-            for (int i1 = 0; i1 < 2; i1++) {
-                //
-                dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
-                createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
-                createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
-                sleep(29000);
-            }
-            //
-            notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
-            createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
-            createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
-            //
-            chartUpdateHeaders = authHeaders("GET", chartUpdateUrl);
-            createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
-            createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
-        }
-
-    }
-
-
-
-
     public void kibanaDashboardRefreshCycle(){ //NOT FINISHED YET!
         String dashboardId = "c020c7c1-2d8c-46f6-933a-abb933788732";
         String preferenceTimeStamp = "1490780679128";
@@ -421,6 +349,7 @@ public class RequestManager extends SignAWSv4{
             e.printStackTrace();
         }
     }
+
 
     private void sleep(int mills){
         try {
---
+++ src/main/java/load/RequestTemplates.java
@@ -0,0 +1,113 @@
+package load;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+/**
+ * Created by alex00x6 on 30.03.2017.
+ */
+public class RequestTemplates extends SignAWSv4 {
+
+     void canvasChartRefreshTemplate(int operatingTimeMins, String chartUpdateUrl, String dashboardInfoUrl, String notificationUnreadUrl){
+        //all requests on start
+        //6 chart update requests every 5 seconds
+        //2 dash info requests every 30 seconds
+        //2 notification requests every 1 min
+        Map<String, String> standardHeaders = standardHeaders();
+        Map<String, String> notificationUnreadHeaders = null;
+        Map<String, String> dashboardInfoHeaders = null;
+        Map<String, String> chartUpdateHeaders = null;
+
+        System.out.println("TIME : HTTP STATUS CODE : RESPONSE TIME : METHOD : URL");
+        System.out.println(LocalDateTime.now() + ": Started");
+        //2 1min requests on start here
+        notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
+        createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
+        createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
+        //2 30 sec requests on start here
+        dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
+        createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
+        createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
+
+        for (int i2 = 0; i2 < operatingTimeMins; i2++) {
+            for (int i1 = 0; i1 < 2; i1++) {
+                for (int i = 0; i < 6; i++) {
+                    //5 sec cycle
+                    //6 requests every 5 sec here (1 sec cut for response)
+                    chartUpdateHeaders = authHeaders("GET", chartUpdateUrl);
+                    createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
+                    createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
+                    createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
+                    createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
+                    createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
+                    createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
+                    sleep(4000);
+                }
+                //30 sec cycle
+                //2 requests every 30 sec here
+                dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
+                createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
+                createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
+            }
+            //1 min cycle
+            //2 requests every 1 min here
+            notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
+            createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
+            createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
+        }
+    }
+
+     void canvasGPVChartRefreshTemplate(int operatingTimeMins, String chartUpdateUrl, String dashboardInfoUrl, String notificationUnreadUrl){
+        //all requests on start
+        //2 dash info requests every 30 sec
+        //2 notification requests every 1 min
+        //2 chart update requests every 1 min
+        Map<String, String> standardHeaders = standardHeaders();
+        Map<String, String> notificationUnreadHeaders = null;
+        Map<String, String> dashboardInfoHeaders = null;
+        Map<String, String> chartUpdateHeaders = null;
+
+        System.out.println("TIME : HTTP STATUS CODE : RESPONSE TIME : METHOD : URL");
+        System.out.println(LocalDateTime.now() + ": Started");
+        //
+        notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
+        createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
+        createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
+        //
+        dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
+        createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
+        createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
+        //
+        chartUpdateHeaders = authHeaders("GET", chartUpdateUrl);
+        createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
+        createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
+
+        for (int i2 = 0; i2 < operatingTimeMins; i2++) {
+            for (int i1 = 0; i1 < 2; i1++) {
+                //
+                dashboardInfoHeaders = authHeaders("GET", dashboardInfoUrl);
+                createEmptyRequestWithHeaders(standardHeaders).options(dashboardInfoUrl);
+                createEmptyRequestWithHeaders(standardHeaders).addHeaders(dashboardInfoHeaders).get(dashboardInfoUrl);
+                sleep(29000);
+            }
+            //
+            notificationUnreadHeaders = authHeaders("GET", notificationUnreadUrl);
+            createEmptyRequestWithHeaders(standardHeaders).options(notificationUnreadUrl);
+            createEmptyRequestWithHeaders(standardHeaders).addHeaders(notificationUnreadHeaders).get(notificationUnreadUrl);
+            //
+            chartUpdateHeaders = authHeaders("GET", chartUpdateUrl);
+            createEmptyRequestWithHeaders(standardHeaders).options(chartUpdateUrl);
+            createEmptyRequestWithHeaders(standardHeaders).addHeaders(chartUpdateHeaders).get(chartUpdateUrl);
+        }
+
+    }
+
+
+    private void sleep(int mills){
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
