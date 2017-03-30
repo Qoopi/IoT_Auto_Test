a93f94f527a3afb9b3ba1c92132c3f121423ccc5
alex00x6 <alex00x6@gmail.com>
2017-03-30 17:48:27 +0300

    CRUD scenario in progress

--- src/test/java/load/PageBootSpeed.java
+++ src/test/java/load/PageBootSpeed.java
@@ -1,24 +1,10 @@
 package load;
 
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import ui.pageObjets.LogInPage;
-import ui.utils.TestListener;
 
-@Listeners(TestListener.class)
+@Listeners(LoadListener.class)
 public class PageBootSpeed {
-
-    @BeforeTest
-    public void getCreds(){
-        LogInPage log = new LogInPage();
-        log.getToIoTPage();
-        log.enterGoogleCred();
-        log.getRequestSigns();
-        RequestManager requestManager = new RequestManager();
-        requestManager.writeCredsTofile();
-        requestManager.setUpBaseApiGateway();
-    }
 
     @Test
     public void checkNewCreds0(){
