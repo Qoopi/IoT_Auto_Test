425417afb2b2f37944f56c62f77db69de164e488
Kutafin Oleg <geloksmmm@gmail.com>
2017-05-04 11:33:54 +0300

    Dashboard test

--- src/test/java/api/DashboardTests.java
+++ src/test/java/api/DashboardTests.java
@@ -22,26 +22,58 @@ public class DashboardTests {
 
     @Test
     public void DashboardCRUDVPVKibana(){
-
+        RequestManagerAPI requestManagerAPI = new RequestManagerAPI();
+        requestManagerAPI.dashboardCreate(requestManagerAPI.jsonDashboardCreateVPVKibana());
+        requestManagerAPI.checkDashboardCreated();
+//        requestManagerAPI.dashboardUpdate(requestManagerAPI.jsonDashboardUpdateVPVCanvas());
+//        requestManagerAPI.checkDashboardUpdated(requestManagerAPI.jsonDashboardUpdatedVPVCanvas());
+        requestManagerAPI.dashboardDelete();
+        requestManagerAPI.checkDashboardDeleted();
     }
 
     @Test
     public void DashboardCRUDGPVKibana255(){
-
+        RequestManagerAPI requestManagerAPI = new RequestManagerAPI();
+        requestManagerAPI.dashboardCreate(requestManagerAPI.jsonDashboardCreateGPVKibana0255());
+        requestManagerAPI.checkDashboardCreated();
+//        requestManagerAPI.dashboardUpdate(requestManagerAPI.jsonDashboardUpdateVPVCanvas());
+//        requestManagerAPI.checkDashboardUpdated(requestManagerAPI.jsonDashboardUpdatedVPVCanvas());
+        requestManagerAPI.dashboardDelete();
+        requestManagerAPI.checkDashboardDeleted();
     }
 
     @Test
     public void DashboardCRUDGPVKibana25(){
-
+        RequestManagerAPI requestManagerAPI = new RequestManagerAPI();
+        requestManagerAPI.dashboardCreate(requestManagerAPI.jsonDashboardCreateGPVKibana025());
+        requestManagerAPI.checkDashboardCreated();
+//        requestManagerAPI.dashboardUpdate(requestManagerAPI.jsonDashboardUpdateVPVCanvas());
+//        requestManagerAPI.checkDashboardUpdated(requestManagerAPI.jsonDashboardUpdatedVPVCanvas());
+        requestManagerAPI.dashboardDelete();
+        requestManagerAPI.checkDashboardDeleted();
     }
+
 
     @Test
     public void DashboardCRUDGPVCanvas25(){
+        RequestManagerAPI requestManagerAPI = new RequestManagerAPI();
+        requestManagerAPI.dashboardCreate(requestManagerAPI.jsonDashboardCreateGPVCanvas025());
+        requestManagerAPI.checkDashboardCreated();
+//        requestManagerAPI.dashboardUpdate(requestManagerAPI.jsonDashboardUpdateVPVCanvas());
+//        requestManagerAPI.checkDashboardUpdated(requestManagerAPI.jsonDashboardUpdatedVPVCanvas());
+        requestManagerAPI.dashboardDelete();
+        requestManagerAPI.checkDashboardDeleted();
 
     }
 
     @Test
     public void DashboardCRUDGPVCanvas255(){
-
+        RequestManagerAPI requestManagerAPI = new RequestManagerAPI();
+        requestManagerAPI.dashboardCreate(requestManagerAPI.jsonDashboardCreateGPVCanvas0255());
+        requestManagerAPI.checkDashboardCreated();
+//        requestManagerAPI.dashboardUpdate(requestManagerAPI.jsonDashboardUpdateVPVCanvas());
+//        requestManagerAPI.checkDashboardUpdated(requestManagerAPI.jsonDashboardUpdatedVPVCanvas());
+        requestManagerAPI.dashboardDelete();
+        requestManagerAPI.checkDashboardDeleted();
     }
 }
