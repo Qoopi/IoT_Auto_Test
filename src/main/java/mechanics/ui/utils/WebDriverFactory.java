32ede2e4f75cd787afaba6002dab61e9ba6d3c0e
Alex Storm <alex00x6@gmail.com>
2017-05-15 14:13:13 +0300

    .jar packaging in progress

--- src/main/java/mechanics/ui/utils/WebDriverFactory.java
+++ src/main/java/mechanics/ui/utils/WebDriverFactory.java
@@ -10,11 +10,15 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverFactory {
+    private static final String unpackedPrefix = "";
+    private static final String windowsPathPrefix = "src/main/resources/selenium/windows/";
+    private static final String linuxPathPrefix = "src/main/resources/selenium/linux/";
 
     public static WebDriver createInstance(String browserName, Boolean useGrid) {
         URL hostURL = null;
@@ -44,44 +48,48 @@ public class WebDriverFactory {
             if (SystemUtils.IS_OS_WINDOWS) {
                 System.out.println("OS is Windows");
                 if (browserName.toLowerCase().contains("firefox")) {
-                    System.setProperty("webdriver.gecko.driver", "geckodriver32.exe");
-//                    System.setProperty("webdriver.gecko.driver", "selenium/windows/geckodriver32.exe");
+                    String propertyName = "webdriver.gecko.driver";
+                    String unpackedPath = unpackedPrefix+"geckodriver32.exe";
+                    String projectPath = windowsPathPrefix+"geckodriver32.exe";
+                    pickDriver(propertyName, unpackedPath, projectPath);
                     driver = new FirefoxDriver();
                 }
                 if (browserName.toLowerCase().contains("ie32")) {
                     System.setProperty("webdriver.ie.driver", "IEDriverServer32.exe");
-//                    System.setProperty("webdriver.ie.driver", "IEDriverServer32.exe");
                     driver = new InternetExplorerDriver();
                 }
                 if (browserName.toLowerCase().contains("ie64")) {
                     System.setProperty("webdriver.ie.driver", "IEDriverServer64.exe");
-//                    System.setProperty("webdriver.ie.driver", "IEDriverServer64.exe");
                     driver = new InternetExplorerDriver();
                 }
 
                 if (browserName.toLowerCase().contains("chrome")) {
-                    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-//                    System.setProperty("webdriver.chrome.driver", "selenium/windows/chromedriver.exe");
+                    String property = "webdriver.chrome.driver";
+                    String unpackedPath = unpackedPrefix+"chromedriver.exe";
+                    String projectPath = windowsPathPrefix+"chromedriver.exe";
+                    pickDriver(property, unpackedPath, projectPath);
                     driver = new ChromeDriver();
                 }
                 if (browserName.toLowerCase().contains("edge")) {
                     System.setProperty("webdriver.edge.driver", "MicrosoftWebDriver.exe");
-//                    System.setProperty("webdriver.edge.driver", "selenium/windows/MicrosoftWebDriver.exe");
                     driver = new EdgeDriver();
                 }
             }
         }
         if (SystemUtils.IS_OS_LINUX) {
             System.out.println("OS is Linux");
-            System.out.println("The local driver configuration for linux is not fully implemented yet ");
             if (browserName.toLowerCase().contains("chrome")) {
-                System.setProperty("webdriver.chrome.driver", "chromedriver");
-//                System.setProperty("webdriver.chrome.driver", "selenium/linux/chromedriver");
+                String propertyName = "webdriver.chrome.driver";
+                String unpackedPath = unpackedPrefix+"chromedriver";
+                String projectPath = linuxPathPrefix+"chromedriver";
+                pickDriver(propertyName, unpackedPath, projectPath);
                 driver = new ChromeDriver();
             }
             if (browserName.toLowerCase().contains("firefox")) {
-                System.setProperty("webdriver.gecko.driver", "geckodriver");
-//                System.setProperty("webdriver.gecko.driver", "selenium/linux/geckodriver");
+                String propertyName = "webdriver.gecko.driver";
+                String unpackedPath = unpackedPrefix+"geckodriver";
+                String projectPath = linuxPathPrefix+"geckodriver";
+                pickDriver(propertyName, unpackedPath, projectPath);
                 driver = new FirefoxDriver();
             }
         }
@@ -90,5 +98,19 @@ public class WebDriverFactory {
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         return driver;
+    }
+    
+    private static void pickDriver(String property, String path1, String path2){
+        if (driverExists(path1)){
+            System.setProperty(property, path1);
+        }
+        if (driverExists(path2)){
+            System.setProperty(property,path2);
+        }
+    }
+
+    private static boolean driverExists(String pathToFile){
+        File tmpDir = new File(pathToFile);
+        return tmpDir.exists() && tmpDir.isFile();
     }
 }
--- src/main/java/tests/Workbench.java
+++ src/main/java/tests/Workbench.java
@@ -1,23 +1,25 @@
 package tests;
 
+import mechanics.api.ListenerAPI;
 import mechanics.api.MQTTManagerAPI;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 /**
  * Created by user on 19.04.2017.
  */
-//@Listeners(ListenerAPI.class)
+@Listeners(ListenerAPI.class)
 public class Workbench {
 
 
-
+    @Test
     public void test1(){
         MQTTManagerAPI mqttManagerAPI = new MQTTManagerAPI();
         mqttManagerAPI.triggerGPV();
 //        mqttManagerAPI.triggerGPV2();
     }
 
-    @Test
+//    @Test
     public void test11(){
         double equipCounter = 001;
         System.out.println(equipCounter);
