30dbec1fee3b4f0fbbb8f5b1cf06ae6b6978cf8d
Alex Storm <alex00x6@gmail.com>
2017-05-18 17:12:01 +0300

    added default args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,14 @@ import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
 import org.testng.collections.Lists;
 import mechanics.system.jar.Unpack;
+import ru.yandex.qatools.allure.*;
+import ru.yandex.qatools.allure.config.AllureConfig;
+import ru.yandex.qatools.allure.report.AllureReportBuilder;
+import ru.yandex.qatools.allure.report.AllureReportBuilderException;
+import ru.yandex.qatools.allure.testng.AllureTestListener;
+import ru.yandex.qatools.allure.utils.AllureShutdownHook;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -31,13 +38,27 @@ public class Main {
         Unpack unpack = new Unpack();
         unpack.unpackOnStart();
         argv.setStage();
-        TestListenerAdapter tla = new TestListenerAdapter();
+//        AllureTestListener atl = new AllureTestListener();
+//        TestListenerAdapter tla = new TestListenerAdapter();
         TestNG testng = new TestNG();
         List<String> suites = Lists.newArrayList();
         suites.add("xml/testng.xml");//path to main .xml
         testng.setTestSuites(suites);
         testng.setVerbose(1);
         testng.run();
+
+        File output = new File("target/allure-report/");
+        File results = new File("target/allure-results/");
+        System.out.println(output.getAbsolutePath());
+        try {
+            AllureReportBuilder allureReportBuilder = new AllureReportBuilder(output);
+            allureReportBuilder.unpackFace();
+            allureReportBuilder.processResults(results);
+        } catch (AllureReportBuilderException e) {
+            e.printStackTrace();
+        }
+
+
         //if any test fails - exit code is 1
         if (testng.hasFailure() || testng.hasFailureWithinSuccessPercentage() || testng.hasSkip()) {
             System.exit(1);
